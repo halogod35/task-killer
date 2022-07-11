10b3d7052f3454df02d57cb091ded400d59a70cb
halogod35 <hondadavey@gmail.com>
2022-07-11 18:54:36 -0400

    Add files via upload

---
+++ taskkill.bat
@@ -0,0 +1,23 @@
+::Simple monitor and kill process (exact name)
+@echo off&prompt :&mode con cols=50 lines=10
+
+set processname=wlanext.exe
+
+:loop
+cls&echo Searching for %processname%...
+for /f "tokens=1 delims=," %%a in ('tasklist /fo csv ^|FINDSTR /C:"%processname%"') do call :killprocess %%a
+ping -n 6 127.0.0.1>NUL
+goto :loop
+
+
+:killprocess
+set name=%*
+set name=.,;%name:"=%;,.
+echo %name%|FINDSTR /C:".,;%processname%;,.">nul || goto :eof
+echo. |set /p d=killing %*...
+taskkill /f /im "%*">nul 2>&1
+set err=%errorlevel%
+set success=Success
+if not %err%==0 set success=fail (err code: %err%)
+if %err%==128 set success=fail (process not found)
+echo %success%&goto :eof
